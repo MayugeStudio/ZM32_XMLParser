e2da9d01a83cdcbcda64719c4bf2f6f47de6049c
MayugeStudio <my.favorite.is.energy@gmail.com>
2025-07-24 11:32:21 +0900

    Initial commit

---
+++ ZM32_XMLParser/zm32xml.hpp
@@ -0,0 +1,14 @@
+#pragma once
+// Parsing                         :
+// 1. Attribute class members      :
+// 2. Tag class members            :
+// 3. Document class members       :
+// 4. Introduce document.parse()   :
+// 4.1. Introduce tokenizer class  :
+// 4.2. Introduce parser    class  :
+// User interface                  :
+// 1. Document interface methods   :
+// 2. Tag interface methods        :
+// 3. Attribute interface methods  :
+
+
