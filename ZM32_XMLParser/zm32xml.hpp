6fbfa8f088c8b3812aafc5703689ac3ecca1f78d
MayugeStudio <my.favorite.is.energy@gmail.com>
2025-07-24 11:48:21 +0900

    attributeを定義した！

--- ZM32_XMLParser/zm32xml.hpp
+++ ZM32_XMLParser/zm32xml.hpp
@@ -1,6 +1,8 @@
 #ifndef ZM32XML_HPP
 #define ZM32XML_HPP
 
+#include <string>
+
 // Parsing                         :
 // 1. Attribute class members      : D: kawahara M: siba 2025/07/24 11:45 ~ 12:00
 // 2. Tag class members            :
@@ -12,5 +14,14 @@
 // 1. Document interface methods   :
 // 2. Tag interface methods        :
 // 3. Attribute interface methods  :
+
+namespace zm32xml {
+	class attribute {
+	public:
+	private:
+		std::string m_name;
+		std::string m_value;
+	};
+}
 
 #endif // ZM32XML_HPP
