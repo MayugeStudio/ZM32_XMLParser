07470911541a6829714333ce6a6b5f5a18b41f65
MayugeStudio <my.favorite.is.energy@gmail.com>
2025-07-24 12:59:40 +0900

    ファイルコメントを追加

--- ZM32_XMLParser/zm32xml.hpp
+++ ZM32_XMLParser/zm32xml.hpp
@@ -1,15 +1,20 @@
-﻿#ifndef ZM32XML_HPP
-#define ZM32XML_HPP
-
-
-#include <string>
-#include <vector>
+﻿///
+/// @file		zm32xml.hpp
+/// @brief		XMLパーサ
+/// @details	ZM32 ペアプログラミング専科での制作物
+/// 
+/// @author		Kawahara, Shiba
+/// 
+/// @date		2024/07/24
+/// 
+/// @version	0.0.1
+///
 
 
 // Parsing                         :
-// 1. Attribute class members      : D: kawahara M: siba 2025/07/24 11:45 ~ 12:00
-// 2. Tag class members            : D: kawahara M: siba 2025/07/24 12:30 ~ 13:00
-// 3. Document class members       : D: kawahara M: siba 2025/07/24 12:40 ~ 13:00
+// 1. Attribute class members      : D: kawahara M: shiba 2025/07/24 11:45 ~ 12:00
+// 2. Tag class members            : D: kawahara M: shiba 2025/07/24 12:30 ~ 13:00
+// 3. Document class members       : D: kawahara M: shiba 2025/07/24 12:40 ~ 13:00
 // 4. Introduce document.parse()   :
 // 4.1. Introduce tokenizer class  :
 // 4.2. Introduce parser    class  :
@@ -17,6 +22,14 @@
 // 1. Document interface methods   :
 // 2. Tag interface methods        :
 // 3. Attribute interface methods  :
+
+
+#ifndef ZM32XML_HPP
+#define ZM32XML_HPP
+
+
+#include <string>
+#include <vector>
 
 
 namespace zm32xml {
